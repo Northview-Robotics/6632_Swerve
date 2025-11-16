089eeac9ba88cbb0dfdbf4cc0f8caba8d80513e3
RichiEee3704 <ya.richard23@gmail.com>
2025-11-16 18:36:40 -0500

    smart dash yes

--- src/main/java/frc/robot/subsystems/Telemetry.java
+++ src/main/java/frc/robot/subsystems/Telemetry.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
@@ -8,17 +11,30 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class Telemetry extends SubsystemBase{
     private static Telemetry telemetry = null;
     private PowerDistribution pdp;
     private Field2d field2d;
     private drive drivetrain = drive.getInstance();
+    Pigeon2 pigeon;
+
+    //lF module encoder, rF module encoder, rR module encoder, lR module encoder
+    AnalogEncoder encoder1;
+    AnalogEncoder encoder2;
+    AnalogEncoder encoder3;
+    AnalogEncoder encoder4;
 
     private Telemetry(){
         pdp = new PowerDistribution(10, ModuleType.kCTRE);
         field2d = new Field2d();
         //Send field2d to the dashboard
         SmartDashboard.putData("Field ", field2d);
+        pigeon = new Pigeon2(9);
+        encoder1 = new AnalogEncoder(0);
+        encoder2 = new AnalogEncoder(1);
+        encoder3 = new AnalogEncoder(2);
+        encoder4 = new AnalogEncoder(3);
     }
 
     public void update(){
@@ -37,6 +53,23 @@ public class Telemetry extends SubsystemBase{
         //Send current voltage to the dashboard
         double voltage = pdp.getVoltage();
         SmartDashboard.putNumber("Voltage ", voltage);
+
+        //Send current yaw to the dashboard
+        double yaw = pigeon.getYaw().getValueAsDouble();
+        SmartDashboard.putNumber("Yaw ", yaw);
+
+        //Send encoder values to the dashboard
+        double encVal1 = encoder1.get();
+        SmartDashboard.putNumber("Encoder 1 ", encVal1);
+
+        double encVal2 = encoder1.get();
+        SmartDashboard.putNumber("Encoder 2 ", encVal2);
+
+        double encVal3 = encoder1.get();
+        SmartDashboard.putNumber("Encoder 3 ", encVal3);
+
+        double encVal4 = encoder1.get();
+        SmartDashboard.putNumber("Encoder 4 ", encVal4);
 
         //Update robot position on the 2d field inside the dashboard
         field2d.setRobotPose(drivetrain.getRobotPose());
